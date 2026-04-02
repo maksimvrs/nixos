82f70730673440fcad0d49ea724bc9bd895ba83a
Maksim Vorontsov <social.maksim.vrs@gmail.com>
2026-04-02 23:19:01 +0700

    feat: use firefox-addons flake and programs.zen-browser module for extensions

--- flake.nix
+++ flake.nix
@@ -14,19 +14,24 @@
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, zen-browser, ... }:
+  outputs = { self, nixpkgs, home-manager, zen-browser, firefox-addons, ... }:
   let
-    system = "x86_64-linux";
+    system    = "x86_64-linux";
     variables = import ./hosts/thinkpad-x1/variables.nix;
-    zenBrowserPackages = zen-browser.packages.${system};
+    firefoxAddons = firefox-addons.packages.${system};
   in {
     nixosConfigurations.${variables.hostname} = nixpkgs.lib.nixosSystem {
       inherit system;
 
       specialArgs = {
-        inherit variables zenBrowserPackages;
+        inherit variables;
       };
 
       modules = [
@@ -38,8 +43,9 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.backupFileExtension = "backup";
+          home-manager.sharedModules = [ zen-browser.homeManagerModules.default ];
           home-manager.extraSpecialArgs = {
-            inherit variables zenBrowserPackages;
+            inherit variables firefoxAddons;
           };
           home-manager.users.${variables.username} = import ./home/maksim.nix;
         }
--- modules/home-manager/zen-browser.nix
+++ modules/home-manager/zen-browser.nix
@@ -1,28 +1,11 @@
 # modules/home-manager/zen-browser.nix
-# Extensions are force-installed via Firefox policies on first browser launch.
-# Zen Browser reads policies from ~/.zen/distribution/policies.json
-{ zenBrowserPackages, ... }: {
-  home.packages = [ zenBrowserPackages.default ];
-
-  home.file.".zen/distribution/policies.json".text = builtins.toJSON {
-    policies = {
-      ExtensionSettings = {
-        # Bitwarden
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          install_url        = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode  = "force_installed";
-        };
-        # SponsorBlock
-        "sponsorBlocker@ajay.app" = {
-          install_url        = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
-          installation_mode  = "force_installed";
-        };
-        # uBlock Origin
-        "uBlock0@raymondhill.net" = {
-          install_url        = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode  = "force_installed";
-        };
-      };
-    };
+{ firefoxAddons, ... }: {
+  programs.zen-browser = {
+    enable = true;
+    profiles.default.extensions.packages = with firefoxAddons; [
+      ublock-origin
+      sponsorblock
+      bitwarden
+    ];
   };
 }
