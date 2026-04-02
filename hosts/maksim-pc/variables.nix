ff47fd3e23294838e28593efe32fe8f9d2ce9931
Maksim Vorontsov <social.maksim.vrs@gmail.com>
2026-04-02 22:00:42 +0700

    feat: add host variables and hardware-configuration placeholder

---
+++ hosts/maksim-pc/variables.nix
@@ -0,0 +1,9 @@
+# hosts/maksim-pc/variables.nix
+{
+  username = "maksim";
+  hostname = "maksim-pc";
+  timezone = "Asia/Almaty";
+  gitName = "Maksim Vorontsov";
+  gitEmail = "maksim.vorontsov@zemomedia.com";
+  stateVersion = "25.05";
+}
