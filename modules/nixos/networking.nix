bd37d8cf5f01e0697e2b60f0745937bad548bad7
Maksim Vorontsov <social.maksim.vrs@gmail.com>
2026-04-03 18:46:16 +0700

    OpenVPN pluging

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,8 +1,9 @@
 # modules/nixos/networking.nix
-{ variables, ... }: {
+{ variables, pkgs, ... }: {
   networking.hostName = variables.hostname;
   networking.networkmanager = {
     enable = true;
+    plugins = [ pkgs.networkmanager-openvpn ];
     wifi = {
       powersave = false;         # prevent WiFi from dropping on idle
       scanRandMacAddress = false; # MAC randomization can break some APs
