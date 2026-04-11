a8622ae66304e657e18f05fa12cfd94a52e854cb
Maksim Vorontsov <social.maksim.vrs@gmail.com>
2026-04-11 11:59:00 +0700

    Fix qtile config

--- home/dotfiles/qtile/helpers/__init__.py
+++ home/dotfiles/qtile/helpers/__init__.py
@@ -8,17 +8,20 @@ power_profile, notify). This ``__init__`` re-exports the names used by
 from .audio import volume_down, volume_mute, volume_up
 from .brightness import brightness_down, brightness_up
 from .mic import mic_mute
-from .power_profile import power_profile_cycle
+from .power_profile import POWER_PROFILE_ICONS, get_power_profile, power_profile_cycle
 from .session import lock_screen, power_menu
-from .window import resize_focused, toggle_floating_centered
+from .window import resize_and_center, resize_focused, toggle_floating_centered
 
 __all__ = [
+    "POWER_PROFILE_ICONS",
     "brightness_down",
     "brightness_up",
+    "get_power_profile",
     "lock_screen",
     "mic_mute",
     "power_menu",
     "power_profile_cycle",
+    "resize_and_center",
     "resize_focused",
     "toggle_floating_centered",
     "volume_down",
