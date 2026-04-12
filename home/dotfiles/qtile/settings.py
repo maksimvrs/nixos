01a7a31ba4e47e8f4a98f55dcbdd37b52b962a6c
Maksim Vorontsov <social.maksim.vrs@gmail.com>
2026-04-12 14:46:43 +0700

    Dont follow cursor

--- home/dotfiles/qtile/settings.py
+++ home/dotfiles/qtile/settings.py
@@ -6,7 +6,7 @@ from theme import layout_theme
 
 dgroups_key_binder = None
 dgroups_app_rules = []
-follow_mouse_focus = True
+follow_mouse_focus = False
 bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
